03d0623a23bc4fc5dc376f3eac9fac5ab846a4d2
charan satya <charan.s@blockcube.co>
2023-02-17 18:54:18 +0530

    refactoring

--- Sample.py
+++ Sample.py
@@ -1,33 +1,31 @@
-
-
 class Employee:
-     def __init__(self,first,last):
-         self.first = first
-         self.last = last
-         # self.email = first+"."+last+"@email.com"
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        # self.email = first+"."+last+"@email.com"
 
-     @property
-     def email(self):
-         return "{}.{}@gmail.com".format(self.first,self.last)
+    @property
+    def email(self):
+        return "{}.{}@gmail.com".format(self.first, self.last)
 
-     @property
-     def fullname(self):
-        return "{} {}".format(self.first,self.last)
+    @property
+    def fullname(self):
+        return "{} {}".format(self.first, self.last)
 
-     @fullname.setter
-     def fullname(self,name):
-         first,last = name.split(" ")
-         self.first = first
-         self.last = last
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(" ")
+        self.first = first
+        self.last = last
 
-     @fullname.deleter
-     def fullname(self):
-         print("Deleted Successfully")
-         self.first = None
-         self.last = None
+    @fullname.deleter
+    def fullname(self):
+        print("Deleted Successfully")
+        self.first = None
+        self.last = None
 
 
-emp_1 = Employee("Jhon","Smith")
+emp_1 = Employee("John", "Smith")
 
 emp_1.fullname = "Virat Kohli"
 
